27dbde4298c03c9d9bbfbc6ee16e4102b633f999
Administrator <17049185@qq.com>
2017-05-11 15:14:47 +0800

    new

---
+++ src/springTest/HelloWorldTest.java
@@ -0,0 +1,10 @@
+package springTest;
+
+public class HelloWorldTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("世界，你好！");
+	}
+
+}
